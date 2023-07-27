177664f93647374577b180daa984e086161930c7
LuizBoina <luizfelipeboina@outlook.com>
2023-07-27 11:23:17 -0300

    last changes

--- 01_command_injection/ex1.c
+++ 01_command_injection/ex1.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-  char input[MAX_BUFFER_SIZE - 4], command[MAX_BUFFER_SIZE];
+  char input[MAX_BUFFER_SIZE], command[MAX_BUFFER_SIZE];
   printf("enter a filename on this path to get the content: \n");
   fgets(input, MAX_BUFFER_SIZE, stdin);
   snprintf(command, MAX_BUFFER_SIZE, "cat %s", input);
